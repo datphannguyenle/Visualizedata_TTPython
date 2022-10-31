0a0615710dacb78cd736c9cbdee7111cce377062
datphannguyenle <datphannguyenle@gmail.com>
2022-10-31 23:19:21 +0700

    clean file
    11:18 31/10/2002

--- offical/main.py
+++ offical/main.py
@@ -1,12 +1,12 @@
 import sys
-import numpy as np
-from ui_main import*
-from PySide2 import QtCore
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-import matplotlib.pyplot as plt
-import pandas as pd
 import requests
+import numpy as np
+import pandas as pd
+from ui_main import*
 import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
 datetime = []
 temp = []
 humidity = []
@@ -14,28 +14,45 @@ windspeed = []
 link = ""
 mode = 0
 
-
 class MiApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow() 
         self.ui.setupUi(self)
-#Nhận biết kích hoạt button
+        #Nhận biết kích hoạt button
         self.ui.load_button.clicked.connect(self.bt_load)
         self.ui.start_button.clicked.connect(self.bt_start)
         self.ui.exit_button.clicked.connect(QApplication.instance().quit)
         self.show()    
-#Thiết lập hoạt động cho load_button
+
+    #Hàm vẽ biểu đồ lên màn hình   
+    def draw(self):
+        self.grafica = Canvas_grafica()
+        self.grafica1 = Canvas_grafica2()
+        self.grafica2 = Canvas_grafica3()
+        self.grafica3 = Canvas_grafica4()
+        self.ui.grafica_uno.addWidget(self.grafica)
+        self.ui.grafica_dos.addWidget(self.grafica1)
+        self.ui.grafica_tres.addWidget(self.grafica2)
+        self.ui.grafica_cuatro.addWidget(self.grafica3)
+
+    #Hàm thiết lập lại editLine
+    def seteditline(self):
+        self.ui.lineEdit.clear()
+        self.ui.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Please input city name", None))
+        self.ui.lineEdit.setStyleSheet(u"background-color: rgb(255, 255, 255);\n""border-radius:5px;\n""color : black")
+
+    #Thiết lập hoạt động cho load_button
     def bt_load(self):
         global link
         diachi = QFileDialog.getOpenFileNames()
         chuanhoa = str(diachi)[:-20]
         chuanhoa = chuanhoa[3:] 
         link = chuanhoa
-        #self.ui.lineEdit.setText(link)
         self.ui.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow",link, None))
         self.ui.lineEdit.setStyleSheet(u"background-color: rgb(255, 255, 255);\n""border-radius:5px;\n""color : red")
-#Thiết lập hoạt động cho start_button
+
+    #Thiết lập hoạt động cho start_button
     def bt_start(self):
         global link
         global datetime
@@ -43,7 +60,7 @@ class MiApp(QMainWindow):
         global humidity
         global windspeed
         global mode
-#Xóa data cũ, đặt trong try sẽ trừ trường hợp lỗi
+        #Xóa data cũ, đặt trong try sẽ trừ trường hợp lỗi
         try:
             self.ui.grafica_uno.removeWidget(self.grafica)
             self.ui.grafica_dos.removeWidget(self.grafica1)
@@ -51,46 +68,25 @@ class MiApp(QMainWindow):
             self.ui.grafica_cuatro.removeWidget(self.grafica3)
         except:
            pass
-#Nếu nhấn start_button mà chưa nhập file mà tên thành phố thì sẽ ưu tiên nhập file
+        #Nếu nhấn start_button mà chưa nhập file mà tên thành phố thì sẽ ưu tiên nhập file
         if (link == "") and (self.ui.lineEdit.text() == ""):
-            diachi = QFileDialog.getOpenFileNames()
-            chuanhoa = str(diachi)[:-20]
-            chuanhoa = chuanhoa[3:] 
-            link = chuanhoa
-            # self.ui.lineEdit.setText(link)
-            # self.ui.lineEdit.setStyleSheet(u"background-color: rgb(255, 255, 255);\n""border-radius:5px;\n""color : red")
+            self.bt_load()
             i = 0
-#Đặt trong try để trừ trường hợp bị lỗi phần mềm do data không đúng khi load dữ liệu vẽ
+            #Đặt trong try để trừ trường hợp bị lỗi phần mềm do data không đúng khi load dữ liệu vẽ
             try:
-                self.grafica = Canvas_grafica()
-                self.grafica1 = Canvas_grafica2()
-                self.grafica2 = Canvas_grafica3()
-                self.grafica3 = Canvas_grafica4()
-                self.ui.grafica_uno.addWidget(self.grafica)
-                self.ui.grafica_dos.addWidget(self.grafica1)
-                self.ui.grafica_tres.addWidget(self.grafica2)
-                self.ui.grafica_cuatro.addWidget(self.grafica3)
+                self.draw()
                 link = ""
             except:
                 pass
-#Nếu đã có link thì sẽ ưu tiên vẽ link
+        #Nếu đã có link thì sẽ ưu tiên vẽ link
         elif link != "":
             try:
-                self.grafica = Canvas_grafica()
-                self.grafica1 = Canvas_grafica2()
-                self.grafica2 = Canvas_grafica3()
-                self.grafica3 = Canvas_grafica4()
-                self.ui.grafica_uno.addWidget(self.grafica)
-                self.ui.grafica_dos.addWidget(self.grafica1)
-                self.ui.grafica_tres.addWidget(self.grafica2)
-                self.ui.grafica_cuatro.addWidget(self.grafica3)
+                self.draw()
                 link = ""
-                self.ui.lineEdit.clear()
-                self.ui.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Please input city name", None))
-                self.ui.lineEdit.setStyleSheet(u"background-color: rgb(255, 255, 255);\n""border-radius:5px;\n""color : black")
+                self.seteditline()
             except:
                 pass
-#Trường hợp còn lại lấy dữ liệu API thời gian thực theo tên thành phố để vẽ
+        #Trường hợp còn lại lấy dữ liệu API thời gian thực theo tên thành phố để vẽ
         else:
             try:
                 api_key = 'ba6a67a16c5be47d1383d19b93965582'
@@ -105,29 +101,21 @@ class MiApp(QMainWindow):
                     windspeed.append(float(response['list'][time_to_get]['wind']['speed']))
                 mode = 1
                 print(windspeed)
-                self.grafica = Canvas_grafica()
-                self.grafica1 = Canvas_grafica2()
-                self.grafica2 = Canvas_grafica3()
-                self.grafica3 = Canvas_grafica4()
+                self.draw()
                 mode = 0
-                self.ui.grafica_uno.addWidget(self.grafica)
-                self.ui.grafica_dos.addWidget(self.grafica1)
-                self.ui.grafica_tres.addWidget(self.grafica2)
-                self.ui.grafica_cuatro.addWidget(self.grafica3)
                 datetime = []
                 temp= []
                 humidity = []
                 windspeed = []
-                self.ui.lineEdit.clear()
-                self.ui.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Please input city name", None))
-                self.ui.lineEdit.setStyleSheet(u"background-color: rgb(255, 255, 255);\n""border-radius:5px;\n""color : black")
-#Vẽ không thành công sẽ thông báo data lỗi
+                self.seteditline()
+            #Vẽ không thành công sẽ thông báo data lỗi
             except:
                 print("Data loi")
                 #self.ui.lineEdit.setText("Error: Please input right city name")
                 self.ui.lineEdit.clear()
                 self.ui.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Error: Please right input city name", None))
                 self.ui.lineEdit.setStyleSheet(u"background-color: rgb(255, 255, 255);\n""border-radius:5px;\n""color : red")
+
 
 #Biêu đồ line
 class Canvas_grafica(FigureCanvas):
@@ -166,6 +154,7 @@ class Canvas_grafica(FigureCanvas):
             except:
                 pass
 
+
 #Biêu đồ scatter
 class Canvas_grafica2(FigureCanvas):
     def __init__(self, parent=None):     
@@ -202,6 +191,7 @@ class Canvas_grafica2(FigureCanvas):
                 plt.ylabel("Index") 
             except:
                 pass
+
 
 #Biêu đồ bar
 class Canvas_grafica3(FigureCanvas):
@@ -240,6 +230,7 @@ class Canvas_grafica3(FigureCanvas):
             except:
                 pass
 
+
 #Biểu đồ histogram
 class Canvas_grafica4(FigureCanvas):
     def __init__(self, parent=None):     
@@ -269,6 +260,7 @@ class Canvas_grafica4(FigureCanvas):
             plt.xticks(fontsize = 6, rotation = 29)
             self.fig.suptitle("Histogram: Raining precipitaion for 14 days",size=9)
             plt.legend(loc='best')
+
 
 #Bắt đấu chương trình chính
 if __name__ == "__main__":
--- offical/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-                self.grafica3 = Canvas_grafica4()
-                mode = 0
-                self.ui.grafica_uno.addWidget(self.grafica)
